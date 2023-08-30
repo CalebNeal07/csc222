fe47f0f777f84385a2fb39476ae91a633fedfc33
CalebNeal07 <caleboneal07@gmail.com>
2023-08-30 08:52:45 -0400

    Week 1 classwork

---
+++ week_1/hello_world.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num = 42;
+
+  cout << "The meaning of life, the universe, and everything";
+  cout << " is of course " << num << "." << endl;
+
+  return 0;
+}
+
