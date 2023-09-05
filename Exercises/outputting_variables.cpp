49240bb47564420d7ed6738e84816ffdffd815f9
CalebNeal07 <caleboneal07@gmail.com>
2023-09-05 08:54:07 -0400

    Finished chapter 2, exercise 1

---
+++ Exercises/outputting_variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int hour, minute;
+  char colon;
+
+  hour = 11;
+  minute = 59;
+  colon = ':';
+
+  cout << "The current time is " << hour << colon << minute << endl;
+}
