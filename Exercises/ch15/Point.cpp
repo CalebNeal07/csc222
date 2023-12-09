89361dd3374699ca16deed8d0a99e266a621836f
CalebNeal07 <caleboneal07@gmail.com>
2023-12-08 20:36:05 -0500

    All tests pass

--- Exercises/ch15/Point.cpp
+++ Exercises/ch15/Point.cpp
@@ -1,4 +1,5 @@
 #include "Point.h"
+#include <cmath>
 using namespace std;
 
 Point::Point() {
@@ -11,8 +12,20 @@ Point::Point(double x, double y) {
   this->y = y;
 }
 
+double Point::getX() { return this->x; }
+
+double Point::getY() { return this->y; }
+
+string Point::clean_double_to_string(double d) {
+  std::string str = std::to_string(d);
+  str.erase(str.find_last_not_of('0') + 1, std::string::npos);
+  str.erase(str.find_last_not_of('.') + 1, std::string::npos);
+  return str;
+}
+
 string Point::to_string() {
-  return "(" + std::to_string(this->x) + ", " + std::to_string(this->y) + ")";
+  return "(" + clean_double_to_string(this->x) + ", " +
+         clean_double_to_string(this->y) + ")";
 }
 
 Point Point::operator+(const Point &p) {
@@ -20,9 +33,16 @@ Point Point::operator+(const Point &p) {
 }
 
 Point Point::operator-(const Point &p) {
-  return Point(this->x - p.x, this->y - y);
+  return Point(this->x - p.x, this->y - p.y);
 }
+
+double Point::distance(Point p) {
+  return sqrt(pow(p.getX() - this->x, 2) + pow(p.getY() - this->y, 2));
+}
+
 ostream &operator<<(ostream &os, const Point &p) {
   os << "(" << p.x << ", " << p.y << ")";
   return os;
 }
+
+Point operator*(int i, Point p) { return Point(i * p.getX(), i * p.getY()); }
--- Exercises/ch15/Point.h
+++ Exercises/ch15/Point.h
@@ -6,12 +6,20 @@ class Point {
   double x;
   double y;
 
+  static string clean_double_to_string(double);
+
 public:
   Point();
   Point(double, double);
 
+  double getX();
+  double getY();
+
   Point operator+(const Point &);
   Point operator-(const Point &);
+  double distance(Point);
   string to_string();
   friend ostream &operator<<(ostream &os, const Point &p);
 };
+
+Point operator*(int, Point);
